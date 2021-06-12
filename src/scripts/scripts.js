e7049e3c38913853a690a172f730277c20176397
fedorov_misha <wands180@gmail.com>
2021-06-12 19:07:02 +0300

    added menu animation

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,9 +2,13 @@ let menu = document.getElementById("menu");
 
 let setActive = (predicate) => {
     
-    if(predicate)
+    if(predicate){
         menu.classList.remove("not__visiable");
-    else
-        menu.classList.add("visiable");
+        menu.classList.add("visiable")
+    }
+    else{
+        menu.classList.add("not__visiable");        
+        menu.classList.remove("visiable");
+    }
 
 }
