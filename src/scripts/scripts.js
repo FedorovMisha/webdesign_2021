23a93970dfd962ee4761eaab493ee0f82c9cd4e1
fedorov_misha <wands180@gmail.com>
2021-06-12 22:36:58 +0300

    added animation

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,14 +1,24 @@
 let menu = document.getElementById("menu");
+menu.style.display="none";
 
 let setActive = (predicate) => {
     
     if(predicate){
+        menu.style.display="block";
         menu.classList.remove("not__visiable");
         menu.classList.add("visiable")
+        menu.style.opacity='1';
     }
     else{
-        menu.classList.add("not__visiable");        
-        menu.classList.remove("visiable");
+        menu.classList.remove("visiable");  
+        menu.classList.add("not__visiable");
+        setTimeout(() => { 
+            menu.style.opacity='0';
+            menu.style.display="none";  
+                
+        }, 999);
     }
 
 }
+
+setActive(false);
