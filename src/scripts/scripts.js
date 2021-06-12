e60428d4733fac1ef7f520196d07792ac4e74681
fedorov_misha <wands180@gmail.com>
2021-06-12 22:45:09 +0300

    bug fixes

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,24 +1,27 @@
 let menu = document.getElementById("menu");
 menu.style.display="none";
-
+let t = 0;
 let setActive = (predicate) => {
     
     if(predicate){
         menu.style.display="block";
+        menu.classList.add("visiable");
         menu.classList.remove("not__visiable");
-        menu.classList.add("visiable")
         menu.style.opacity='1';
+        if(t!=null)
+        {
+            clearTimeout(t);
+            t = null;
+        }
     }
     else{
         menu.classList.remove("visiable");  
         menu.classList.add("not__visiable");
-        setTimeout(() => { 
+        t = setTimeout(() => { 
             menu.style.opacity='0';
             menu.style.display="none";  
-                
-        }, 999);
+
+        }, 1000);
     }
 
 }
-
-setActive(false);
